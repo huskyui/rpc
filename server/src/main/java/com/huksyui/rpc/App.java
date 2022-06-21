46390476e90af4061a8da1415268f297494c374c
王鹏 <peng7.wang@ly.com>
2022-06-21 20:14:54 +0800

    多模块

---
+++ server/src/main/java/com/huksyui/rpc/App.java
@@ -0,0 +1,13 @@
+package com.huksyui.rpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
